83a89bf202192857210f0cf449cd00ae952abdb0
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-03-27 15:38:18 +0530

    fixed a typo and a bug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-import express from "express";
-const app = express();
+// import express from "express";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -11,7 +11,7 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(` Server is running at port ${process.env.PORT}`);
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     });
   })
   .catch((err) => {
