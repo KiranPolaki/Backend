b98a52bf72422ecedccdd9ddc00e56f5070fa9bd
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-03-27 15:59:08 +0530

    steps to register was included

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  //Get user details from front end/ postman
+  //validation - not empty
+  //check if user already exists - check email, username
+  //Required files are provided images, avatar
+  //upload them to cloudinary - get url from the response (validate avatar)
+  //create user object - create entry in DB
+  //remove password and refreshTOken fields from response
+  //check for user creation response
+  //return response
 });
 
 export { registerUser };
