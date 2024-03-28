76d16f1fa4092634a259733de67c2c22fa66b0da
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-03-28 11:14:52 +0530

    updated ApiError FIle

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -1,18 +1,18 @@
 //TODO: read more about the constructors in JS
 class ApiError extends Error {
   constructor(
-    statuscode,
+    statusCode,
     message = "Something went wrong",
     errors = [],
     stack = ""
   ) {
     super(message);
-    this.statuscode = statuscode;
-    //TODO: read more about this.data
+    this.statusCode = statusCode;
     this.data = null;
     this.message = message;
     this.success = false;
     this.errors = errors;
+
     if (stack) {
       this.stack = stack;
     } else {
