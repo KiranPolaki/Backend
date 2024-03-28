6cc2822c53f04a23329cb342c3fd2a0a6f0ced3c
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-03-28 11:10:41 +0530

    add unlinking files after uploading to cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // * file has been uploaded successfully
-    console.log("File is uploaded on Coludinary :) ", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     // ! remove the locally saved temp file as the upload operation got failed do this Syncronously only
